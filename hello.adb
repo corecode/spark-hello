f6329041e7981666589ecce09244a038e3caa368
Simon Schubert <2@0x2c.org>
2020-05-25 17:01:50 +0200

    prove hello

--- hello.adb
+++ hello.adb
@@ -1,6 +1,25 @@
-with Ada.Text_IO;
+pragma Spark_Mode (On);
+
+with SPARK.Text_IO;
+use Spark.Text_Io;
+with Ada.Command_Line;
 
 procedure Hello is
+
+   function Greetee return String is
+   begin
+      if Ada.Command_Line.Argument_Count >= 1 then
+         return Ada.Command_Line.Argument(1);
+      else
+         return "stranger";
+      end if;
+   end Greetee;
+
 begin
-   Ada.Text_IO.Put_Line ("Hello.");
+   if Status (Standard_Output) = Success then
+      Put ("Hello ");
+      if Status (Standard_Output) = Success then
+         Put_Line (Greetee);
+      end if;
+   end if;
 end Hello;
